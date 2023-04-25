63f25d20d728dd5d7f4b36348e291d7ae6aa65a1
mhabeel <121513154+mhabeel@users.noreply.github.com>
2023-04-25 12:40:17 +0200

    ubdate

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "fullstackwebapppatea": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
